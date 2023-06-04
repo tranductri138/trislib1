524bc03860688727d337b24eba16bce85dde247c
trisdepzai <tritocao@gmail.com>
2023-06-04 12:47:50 +0700

    add a thing

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const { reqBank } = require("./src/routes/routes1");
 const jsonParser = bodyParser.json();
 
 app.post("/bank", jsonParser, (req, res) => {
-  console.log(typeof reqBank);
   const result = reqBank(req.body.code);
   res.status(200).send({
     msg: result,
@@ -21,5 +20,3 @@ app.get("/get-user-name", (req, res) => {
 
 app.listen(3000);
 
-
-app.listen(4000)
