860f230fbf5c23aa0487feff92be7322947e0cae
trisdepzai <tritocao@gmail.com>
2023-06-06 23:18:03 +0700

    add

--- src/tableOfContent/goodSentences.js
+++ src/tableOfContent/goodSentences.js
@@ -1,5 +1,7 @@
 const sens1 = 'I will make a distinction between theory and practice.'
 
+const hocTienganh = 'https://prep.vn/blog/lo-trinh-hoc-ngu-phap-tieng-anh/?ref=yt-matgoc'
+
 module.exports = {
     sens1
 }
--- src/tableOfContent/skills.js
+++ src/tableOfContent/skills.js
@@ -16,14 +16,24 @@ const tenSkills = [
 const readSentences = () => {
   console.log(sens1);
 };
+const hackerLaw = "https://github.com/dwmkerr/hacker-laws";
+const refactorCode =
+  "https://github.com/willykraisler/prueba/blob/master/M.Fowler%20et%20al%20-%20Refactoring%20-%20Improving%20the%20Design%20of%20Existing.pdf";
+const cleanCode =
+  "https://github.com/dev-marko/clean-code-book/blob/master/Clean%20Code%20(%20PDFDrive.com%20).pdf";
+const mongodbDesignPattern =
+  "https://pepa.holla.cz/wp-content/uploads/2016/11/MongoDB-Applied-Design-Patterns.pdf";
 
 const read = () => {
   console.table(tenSkills);
   const designPattern =
     "https://github.com/nilgadia/books/blob/master/Design%20Patterns%2C%20Elements%20of%20Reusable%20Object-Oriented%20Software.pdf";
-    const enterpriseIntergate = "https://github.com/pankajchopra/togaf/blob/master/Addison.Wesley.Enterprise.Integration.Patterns.pdf"
+  const enterpriseIntergate =
+    "https://github.com/pankajchopra/togaf/blob/master/Addison.Wesley.Enterprise.Integration.Patterns.pdf";
   exec(`start microsoft-edge:${designPattern}`);
-  exec("start microsoft-edge:"+enterpriseIntergate)
+  exec("start microsoft-edge:" + enterpriseIntergate);
 };
+
+
 
 module.exports = read;
